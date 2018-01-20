758926d99a558846d0d7380d1ec0bf2940a8eb8a
ericstotle93 <elu1993@gmail.com>
2018-01-20 14:39:18 -0500

    Created proxy for react and express

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import IdeaPage from './components/IdeaPage'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        
-      </div>
-    );
+		<div>
+			<IdeaPage/>
+		</div>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ client/src/components/IdeaPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class IdeaPage extends Component {
+    render() {
+        return (
+            <div>Hello From IdeaPage!</div>
+        )
+    }
+}
+
+export default IdeaPage
