4f0dcb4f4e26323a03db6750ffe279941c7a96fe
ericstotle93 <elu1993@gmail.com>
2018-01-20 11:47:15 -0500

    Added concurrently to app

--- server.js
+++ server.js
@@ -7,12 +7,24 @@ const mongoose = require('mongoose')
 const app = express()
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI, {
-    useMongoClient: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 
 const connection = mongoose.connection
 connection.on('connected', () => {
     console.log('Mongoose Connected Successfully!')
+})
+
+connection.on('error', (err) => {
+    console.log(`Mongoose default connection error: ` + err)
+})
+
+app.use(bodyParser.json())
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log('Magic is happening on port ' + PORT)
 })
 
